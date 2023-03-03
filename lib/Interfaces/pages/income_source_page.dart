4338c4561082d6fed0bcd36a50a715bebc93ed04
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2023-03-03 22:16:23 +0700

    Change: Add custom button for income source page

--- lib/Interfaces/pages/income_source_page.dart
+++ lib/Interfaces/pages/income_source_page.dart
@@ -1,5 +1,6 @@
 import 'package:financial_tracker/Commons/themes/colors.dart';
 import 'package:financial_tracker/Domains/sources/entities/source.dart';
+import 'package:financial_tracker/Interfaces/widgets/button_custom.dart';
 import 'package:financial_tracker/Interfaces/widgets/income_source_page/income_source_tile.dart';
 import 'package:financial_tracker/Interfaces/widgets/list_tile_custom.dart';
 import 'package:flutter/material.dart';
@@ -17,32 +18,49 @@ class _IncomeSourcePageState extends State<IncomeSourcePage> {
     Source(id: 1, name: "Bank One", imageRoute: "assets/images/logo.png"),
     Source(id: 1, name: "Bank Two", imageRoute: "assets/images/logo.png"),
     Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
+    Source(id: 1, name: "Bank Three", imageRoute: "assets/images/logo.png"),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
-      child: SingleChildScrollView(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Stack(
         children: [
-          ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: sourceList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTileCustom(
-                  color: themeColor.secondary,
-                  child: IncomeSourceTile(
-                    incomeSourceName: sourceList[index].name,
-                    imageRoute: sourceList[index].imageRoute,
-                    onTap: () {},
-                  ),
-                );
-              }),
+          SingleChildScrollView(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: sourceList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTileCustom(
+                      color: themeColor.secondary,
+                      child: IncomeSourceTile(
+                        incomeSourceName: sourceList[index].name,
+                        imageRoute: sourceList[index].imageRoute,
+                        onTap: () {},
+                      ),
+                    );
+                  }),
+            ],
+          )),
+          Positioned(
+            bottom: 0.0,
+            child: ButtonCustom(
+                icon: Icons.add, text: "Add New Income Source", onTap: () {}),
+          ),
         ],
-      )),
+      ),
     );
   }
 }
---
+++ lib/Interfaces/widgets/button_custom.dart
@@ -0,0 +1,48 @@
+import 'package:financial_tracker/Commons/themes/colors.dart';
+import 'package:flutter/material.dart';
+
+class ButtonCustom extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final void Function() onTap;
+
+  const ButtonCustom(
+      {super.key, required this.icon, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.025,
+            vertical: MediaQuery.of(context).size.height * 0.01),
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.05),
+        height: MediaQuery.of(context).size.height * 0.085,
+        width: MediaQuery.of(context).size.width * 0.95,
+        decoration: BoxDecoration(
+          color: HSLColor.fromColor(themeColor.secondary)
+              .withLightness(0.3)
+              .toColor(),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: themeColor.text,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.01,
+            ),
+            Text(
+              text,
+              style: TextStyle(color: themeColor.text),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
